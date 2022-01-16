f3311e8e695068cead80416cece07b02c42b322f
Rodrigo Roman Salvatierra <rodrigo_00101@hotmail.com>
2022-01-16 00:00:17 -0400

    creacion clase hija Cliente

---
+++ 6-HerenciaCliente.js
@@ -0,0 +1,27 @@
+ class Cliente extends Persona{
+
+    static contadorCliente = 0;
+
+    constructor(fechaRegistro){
+        this._idCliente = ++Cliente.contadorCliente;
+        this._fechaRegistro = fechaRegistro;
+    }
+
+    get idCliente(){
+        return this._idCliente;
+    }
+
+    get fechaRegistro(){
+        return this._fechaRegistro;
+    }
+
+    set fechaRegistro(fecha){
+        this._fechaRegistro = fecha;
+    }
+
+    toString(){
+        return `${super.toString()} 
+                ${this._idCliente} 
+                ${this._fechaRegistro}`;
+    }
+ }
